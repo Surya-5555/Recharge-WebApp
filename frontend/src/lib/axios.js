e9b3e88f4c3ad37c661c442851b62c52cb54dcd1
Surya P R <suryaravichandran5555@gmail.com>
2025-12-19 08:44:04 +0530

    Updated env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5003/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5003/api"
+    : import.meta.env.VITE_BACKEND_URL;
+
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
